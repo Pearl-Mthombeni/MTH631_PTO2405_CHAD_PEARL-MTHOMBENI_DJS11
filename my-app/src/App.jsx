2551d8980594acd54d682665a1a4ae1a26b0d21d
Pearl-Mthombeni <pearlmthombeni97@gmail.com>
2025-04-05 11:45:23 +0200

    app.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShowList from "./components/ShowList";
+import ShowDetail from "./components/ShowDetail";
+import LandingPage from "./components/LandingPage";
+import SeasonDetail from "./components/ShowDetailPage";
+import FavoritesPage from "./components/FavoritesPage";
+import AudioPlayer from "./components/PodcastPlayer";
+import { AudioProvider } from "./context/AudioContext";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AudioProvider>
+      {" "}
+      {}
+      <div className="App">
+        <Router>
+          <AudioPlayer /> {}
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/showlist" element={<ShowList />} />
+            <Route path="/show/:showId" element={<ShowDetail />} />
+            <Route
+              path="/show/:showId/season/:seasonIndex"
+              element={<SeasonDetail />}
+            />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </AudioProvider>
+  );
+};
 
-export default App
+export default App;
